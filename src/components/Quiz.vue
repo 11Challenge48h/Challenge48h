2073fb2155e8dba60bee2c323bfff50c4876d16c
Romain Neil <contact@romain-neil.fr>
2022-02-09 00:59:23 +0100

    refactor(Quiz): shuffle answers array

--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -96,6 +96,24 @@ questions.value.push([
 
 const points = ref(0);
 
+function shuffle(array) {
+	let currentIndex = array.length,  randomIndex;
+	
+	// While there remain elements to shuffle...
+	while (currentIndex !== 0) {
+		
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex--;
+		
+		// And swap it with the current element.
+		[array[currentIndex], array[randomIndex]] = [
+			array[randomIndex], array[currentIndex]];
+	}
+	
+	return array;
+}
+
 function createQuestion(category, obj) {
 	let name = obj.name;
 	let attr = "";
@@ -137,11 +155,13 @@ function createQuestion(category, obj) {
 	
 	return [
 		"What is the " + attr + " of " + name + " ?",
-		[
-			[correct, true],
-			[rand(0, 175), false],
-			[rand(190, 20000), false],
-		]
+		shuffle(
+			[
+				[correct, true],
+				[rand(0, 175), false],
+				[rand(190, 20000), false],
+			]
+		)
 	]
 }
 

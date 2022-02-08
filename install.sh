4523dc3b1761b459377f761c97dede67bd0e2cb8
Romain Neil <contact@romain-neil.fr>
2022-02-08 20:49:58 +0100

    feat: build app on server + action related conf

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Build app
+npm run build
+
+#Copy distributing files to proper directory
+sudo cp ./dist/* /var/www/html/
+
+#Set correct rights on destination folder
+sudo chown www-data:www-data -R /var/www/html/
